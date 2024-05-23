295861804e8ca82c7855f85043f805555c9effc2
FedericoL_exo <federicol@exo.com.ar>
2024-05-23 15:01:41 -0300

    Arreglo en chat_page. Seagrego el dispose

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -137,4 +137,14 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
       _isWriten = false;
     });
   }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
+  }
 }
